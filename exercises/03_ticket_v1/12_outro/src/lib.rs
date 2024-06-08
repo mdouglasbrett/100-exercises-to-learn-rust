1db275580885525cbbc8c510ed498746cce48ad6
Michael Brett <mdouglasbrett@icloud.com>
2024-06-08 19:40:43 +0100

    wip: ticket_v1 wrap-up solution

--- exercises/03_ticket_v1/12_outro/src/lib.rs
+++ exercises/03_ticket_v1/12_outro/src/lib.rs
@@ -11,3 +11,73 @@
 // Integration here has a very specific meaning: they test **the public API** of your project.
 // You'll need to pay attention to the visibility of your types and methods; integration
 // tests can't access private or `pub(crate)` items.
+fn validate_quantity(quantity: u32) {
+    if quantity == 0 {
+        panic!("The quantity must be strictly greater than zero");
+    }
+}
+
+// TODO: newtype for Cents?
+fn validate_unit_price(unit_price: u32) {
+    if unit_price == 0 {
+        panic!("The item price must be strictly greater than zero");
+    }
+}
+
+fn validate_product_name(product_name: &str) {
+    if product_name.is_empty() {
+        panic!("Product name cannot be empty");
+    }
+    if product_name.len() > 300 {
+        panic!("Product name cannot be greater than 300 bytes");
+    }
+}
+pub struct Order {
+    product_name: String,
+    quantity: u32,
+    unit_price: u32,
+}
+
+impl Order {
+    pub fn new(product_name: String, quantity: u32, unit_price: u32) -> Self {
+        validate_quantity(quantity);
+        validate_unit_price(unit_price);
+        validate_product_name(&product_name);
+
+        Order {
+            product_name,
+            unit_price,
+            quantity,
+        }
+    }
+    pub fn total(&self) -> u32 {
+        let total = self.quantity.saturating_mul(self.unit_price);
+        total
+    }
+    pub fn product_name(&self) -> &String {
+        &self.product_name
+    }
+
+    pub fn quantity(&self) -> &u32 {
+        &self.quantity
+    }
+
+    pub fn unit_price(&self) -> &u32 {
+        &self.unit_price
+    }
+
+    pub fn set_product_name(&mut self, new_product_name: String) {
+        validate_product_name(&new_product_name);
+        self.product_name = new_product_name;
+    }
+
+    pub fn set_quantity(&mut self, new_quantity: u32) {
+        validate_quantity(new_quantity);
+        self.quantity = new_quantity;
+    }
+
+    pub fn set_unit_price(&mut self, new_unit_price: u32) {
+        validate_unit_price(new_unit_price);
+        self.unit_price = new_unit_price;
+    }
+}
