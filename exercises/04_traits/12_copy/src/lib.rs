60a6eb9efd9a67083ac37aa4b23f600a2ce71724
Michael Brett <mdouglasbrett@icloud.com>
2024-06-09 11:42:19 +0100

    wip: copy solution

--- exercises/04_traits/12_copy/src/lib.rs
+++ exercises/04_traits/12_copy/src/lib.rs
@@ -1,6 +1,8 @@
 // TODO: implement the necessary traits to make the test compile and pass.
 //  You *can't* modify the test.
+use std::ops::Add;
 
+#[derive(Clone, Copy, Debug)]
 pub struct WrappingU32 {
     value: u32,
 }
@@ -8,6 +10,21 @@ pub struct WrappingU32 {
 impl WrappingU32 {
     pub fn new(value: u32) -> Self {
         Self { value }
+    }
+}
+
+impl Add for WrappingU32 {
+    type Output = WrappingU32;
+    fn add(self, other: WrappingU32) -> Self::Output {
+        Self {
+            value: other.value.wrapping_add(self.value),
+        }
+    }
+}
+
+impl PartialEq for WrappingU32 {
+    fn eq(&self, other: &Self) -> bool {
+        self.value == other.value
     }
 }
 
