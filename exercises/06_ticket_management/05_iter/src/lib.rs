5613ec1a00c5bd6d0b7c7a437961c3f09bb78893
Michael Brett <mdouglasbrett@icloud.com>
2024-07-22 12:53:58 +0100

    wip: adds iter solution

--- exercises/06_ticket_management/05_iter/src/lib.rs
+++ exercises/06_ticket_management/05_iter/src/lib.rs
@@ -30,6 +30,32 @@ impl TicketStore {
     pub fn add_ticket(&mut self, ticket: Ticket) {
         self.tickets.push(ticket);
     }
+
+    pub fn iter(&self) -> std::slice::Iter<Ticket> {
+        self.tickets.iter()
+    }
+}
+
+impl IntoIterator for TicketStore {
+    type Item = Ticket;
+    type IntoIter = std::vec::IntoIter<Ticket>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        self.tickets.into_iter()
+    }
+}
+
+// @mdouglasbrett - the .iter chapter refers to implementing IntoIterator for
+// a reference to the collection. Although it wasn't a requirement to pass this
+// exercise, I thought I'd have a go at hacking it and writing a (super clunky) 
+// test
+impl IntoIterator for &TicketStore {
+    type Item = Ticket;
+    type IntoIter = std::vec::IntoIter<Ticket>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        self.tickets.clone().into_iter()
+    }
 }
 
 #[cfg(test)]
@@ -41,22 +67,39 @@ mod tests {
     fn add_ticket() {
         let mut store = TicketStore::new();
 
-        let ticket = Ticket {
+        let ticket1 = Ticket {
             title: ticket_title(),
             description: ticket_description(),
             status: Status::ToDo,
         };
-        store.add_ticket(ticket);
+        store.add_ticket(ticket1);
 
-        let ticket = Ticket {
+        let ticket2 = Ticket {
             title: ticket_title(),
             description: ticket_description(),
             status: Status::InProgress,
         };
-        store.add_ticket(ticket);
+        store.add_ticket(ticket2);
 
         let tickets: Vec<&Ticket> = store.iter().collect();
         let tickets2: Vec<&Ticket> = store.iter().collect();
         assert_eq!(tickets, tickets2);
+
+        let test_tickets = vec![
+            Ticket {
+                title: ticket_title(),
+                description: ticket_description(),
+                status: Status::InProgress,
+            },
+            Ticket {
+                title: ticket_title(),
+                description: ticket_description(),
+                status: Status::ToDo,
+            },
+        ];
+
+        for i in &store {
+            assert!(test_tickets.contains(&i));
+        }
     }
 }
