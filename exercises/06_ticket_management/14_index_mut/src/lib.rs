40b06cfb177a0fc63246bb413d93b4c1e3dd7490
Michael Brett <mdouglasbrett@icloud.com>
2024-07-23 17:42:22 +0100

    wip: adds index_mut solution

--- exercises/06_ticket_management/14_index_mut/src/lib.rs
+++ exercises/06_ticket_management/14_index_mut/src/lib.rs
@@ -1,6 +1,6 @@
 // TODO: Implement `IndexMut<&TicketId>` and `IndexMut<TicketId>` for `TicketStore`.
 
-use std::ops::Index;
+use std::ops::{Index, IndexMut};
 use ticket_fields::{TicketDescription, TicketTitle};
 
 #[derive(Clone)]
@@ -57,6 +57,10 @@ impl TicketStore {
     pub fn get(&self, id: TicketId) -> Option<&Ticket> {
         self.tickets.iter().find(|&t| t.id == id)
     }
+
+    pub fn get_mut(&mut self, id: TicketId) -> Option<&mut Ticket> {
+        self.tickets.iter_mut().find(|t| t.id == id)
+    }
 }
 
 impl Index<TicketId> for TicketStore {
@@ -72,6 +76,22 @@ impl Index<&TicketId> for TicketStore {
 
     fn index(&self, index: &TicketId) -> &Self::Output {
         &self[*index]
+    }
+}
+
+// @mdouglasbrett - I originally did this the other way, like I did in the previous
+// exercise, where I implemented the &TicketId version first and then indexed with
+// a reference to TicketId. This way matches the implementation of Index here
+// (using unwrap as well as the main implementation on TicketId)
+impl IndexMut<TicketId> for TicketStore {
+    fn index_mut(&mut self, index: TicketId) -> &mut Self::Output {
+        self.get_mut(index).unwrap()
+    }
+}
+
+impl IndexMut<&TicketId> for TicketStore {
+    fn index_mut(&mut self, index: &TicketId) -> &mut Self::Output {
+        &mut self[*index]
     }
 }
 
