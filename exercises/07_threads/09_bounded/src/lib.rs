b1f9c87e20608111600fe1d18b3165edc04fe0bd
Michael Brett <mdouglasbrett@icloud.com>
2024-08-05 08:25:32 +0100

    wip: starts to flesh out bounded solution
    TODO: I don't think I should be returning the Result, maybe loop on the
    error until it eventually resolves?

--- exercises/07_threads/09_bounded/src/lib.rs
+++ exercises/07_threads/09_bounded/src/lib.rs
@@ -1,40 +1,51 @@
 // TODO: Convert the implementation to use bounded channels.
 use crate::data::{Ticket, TicketDraft};
 use crate::store::{TicketId, TicketStore};
-use std::sync::mpsc::{Receiver, Sender};
+use std::sync::mpsc::{Receiver, SyncSender, TrySendError};
 
 pub mod data;
 pub mod store;
 
 #[derive(Clone)]
 pub struct TicketStoreClient {
-    sender: todo!(),
+    sender: SyncSender<Command>,
+    capacity: usize,
 }
 
 impl TicketStoreClient {
-    pub fn insert(&self, draft: TicketDraft) -> Result<TicketId, todo!()> {
-        todo!()
+    pub fn insert(&self, draft: TicketDraft) -> Result<TicketId, TrySendError<Command>> {
+        let (resp_sender, resp_receiver) = std::sync::mpsc::sync_channel(self.capacity);
+        let msg = Command::Insert {
+            draft,
+            response_channel: resp_sender,
+        };
+        // TODO: this!!! arrgh
+        let err = self.sender.try_send(msg)?;
+        if let Ok(id) = resp_receiver.recv() {
+            return Ok(id);
+        } else {
+            return Err(err);
+        }
+
     }
 
-    pub fn get(&self, id: TicketId) -> Result<Option<Ticket>, todo!()> {
-        todo!()
-    }
+    pub fn get(&self, id: TicketId) -> Result<Option<Ticket>, TrySendError<Command>> {}
 }
 
 pub fn launch(capacity: usize) -> TicketStoreClient {
-    todo!();
+    let (sender, receiver) = std::sync::mpsc::sync_channel(capacity);
     std::thread::spawn(move || server(receiver));
-    todo!()
+    TicketStoreClient { sender, capacity }
 }
 
 enum Command {
     Insert {
         draft: TicketDraft,
-        response_channel: todo!(),
+        response_channel: SyncSender<TicketId>,
     },
     Get {
         id: TicketId,
-        response_channel: todo!(),
+        response_channel: SyncSender<Option<Ticket>>,
     },
 }
 
@@ -47,7 +58,7 @@ pub fn server(receiver: Receiver<Command>) {
                 response_channel,
             }) => {
                 let id = store.add_ticket(draft);
-                todo!()
+                let _ = response_channel.try_send(id);
             }
             Ok(Command::Get {
                 id,
